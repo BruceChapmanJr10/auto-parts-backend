7672531bd10015bf07bfdd95568fd3040a568c53
BruceChapmanJr10 <BruceChapmanJr10@gmail.com>
2026-02-24 21:13:34 -0500

    added search controller

--- src/main/java/com/example/demo/controller/SearchController.java
+++ src/main/java/com/example/demo/controller/SearchController.java
@@ -1,4 +1,25 @@
 package com.example.demo.controller;
 
+import com.example.demo.model.dto.ListingResponse;
+import com.example.demo.service.aggregation.AggregationService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/search")
+@CrossOrigin
 public class SearchController {
+    private final AggregationService aggregationService;
+
+    public SearchController(AggregationService aggregationService) {
+        this.aggregationService = aggregationService;
+    }
+
+    @GetMapping
+    public List<ListingResponse> search(@RequestParam String query) {
+        return aggregationService.searchAllSources(query);
+    }
+
+
 }
