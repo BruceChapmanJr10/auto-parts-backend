7d8a7ec62cd8bda8ed29e8cff505a54a03bac1a4
BruceChapmanJr10 <BruceChapmanJr10@gmail.com>
2026-02-24 22:21:50 -0500

    added amazon to aggegationService

--- src/main/java/com/example/demo/service/aggregation/AggregationService.java
+++ src/main/java/com/example/demo/service/aggregation/AggregationService.java
@@ -2,21 +2,35 @@ package com.example.demo.service.aggregation;
 
 
 import com.example.demo.model.dto.ListingResponse;
+import com.example.demo.service.amazon.AmazonSearchService;
 import com.example.demo.service.ebay.EbaySearchService;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class AggregationService {
 
     private final EbaySearchService ebaySearchService;
+    private final AmazonSearchService amazonSearchService;
 
-    public AggregationService(EbaySearchService ebaySearchService) {
+    public AggregationService(
+            EbaySearchService ebaySearchService,
+            AmazonSearchService amazonSearchService
+    ) {
         this.ebaySearchService = ebaySearchService;
+        this.amazonSearchService = amazonSearchService;
     }
 
     public List<ListingResponse> searchAllSources(String query) {
-        return ebaySearchService.search(query);
+
+        List<ListingResponse> results = new ArrayList<>();
+
+        // Aggregate results from each provider
+        results.addAll(ebaySearchService.search(query));
+        results.addAll(amazonSearchService.search(query));
+
+        return results;
     }
 }
